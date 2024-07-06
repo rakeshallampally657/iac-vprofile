81429ed3d5dcae84e19bbd3b636e504f2cdfa9f3
rakeshallampally657 <rakeshallampally657@gmail.com>
2024-07-06 12:16:39 +0200

    check

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 }
 
 variable "clusterName" {
-  description = "Name of the EKS cluster"
+  description = "Name of the EKS  cluster"
   type        = string
   default     = "vprofile-eks"
 }
